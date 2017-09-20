37b0b5c5e4576a99c2879765193ba5997c2a1196
Breyner Eugenio <breynerg@gmail.com>
2017-09-19 21:56:40 -0300

    Projeto criado

---
+++ src/main/java/com/projectpds/breyner/Pds1DirigidoApplication.java
@@ -0,0 +1,12 @@
+package com.projectpds.breyner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pds1DirigidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pds1DirigidoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/projectpds/breyner/Pds1DirigidoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.projectpds.breyner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Pds1DirigidoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
